8b2d4b8c09d7cba6523dd969a245ae81771c86ce
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-02 12:58:34 -0500

    commit tree c223b71367b816a9c9b9faefef7dc2ca5fc4d0d1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    binding.pry
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+  end
+
+  delete '/articles/:id' do
+    erb :delete
+  end
+
 end
